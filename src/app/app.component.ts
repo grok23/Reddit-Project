f1aebf259f359da76afa8cf9a585ff9de5d91071
grok23 <46896171+grok23@users.noreply.github.com>
2020-02-15 14:35:25 +0000

    added domain function from first app reddit.pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,22 @@ import { Article } from "./article/article.model";
 })
 export class AppComponent {
 
-  articles:Article[];
-  constructor () {
-    this.articles=[
-      new Article('Angular','http://angular.io',5),
-      new Article('Google','http://google.ie',11),
-      new Article('TikTok','http://tiktok.com',-1),  //interestingly, due to the "or 10" votes used in the constructor, setting a base of zero votes here causes a jump to 10 votes
-      new Article('DuckDuckGo','https://duckduckgo.com/',16),
-      new Article('Pi-Hole','https://pi-hole.net/',25)
+  articles: Article[];
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://angular.io', 5),
+      new Article('Google', 'http://google.ie', 11),
+      new Article('JSON', 'https://www.json.org/json-en.html', 12),  //interestingly, due to the "or 10" votes used in the constructor, setting a base of zero votes here causes a jump to 10 votes
+      new Article('DuckDuckGo', 'https://duckduckgo.com/', 16),
+      new Article('Pi-Hole', 'https://pi-hole.net/', 25)
     ];
   }
 
-  addArticle(title:HTMLInputElement, link:HTMLInputElement):boolean {
-  this.articles.push(new Article(title.value, link.value,0));
-  title.value=' ';
-  link.value=' '; 
-  console.log(`Adding Article Title${title.value} : and Adding Article Link ${link.value}: `);
-  return false;
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = ' ';
+    link.value = ' ';
+    console.log(`Adding Article Title${title.value} : and Adding Article Link ${link.value}: `);
+    return false;
   }
 }
--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -1,19 +1,30 @@
 export class Article {
-    votes:number;
-    title:string;
-    link:string;
-  
-    constructor(title:string, link:string, votes?:number) {
-      this.title=title;
-      this.link=link;
-      this.votes=votes ||10;
-     }
+    votes: number;
+    title: string;
+    link: string;
+
+    constructor(title: string, link: string, votes?: number) {
+        this.title = title;
+        this.link = link;
+        this.votes = votes || 10;
+    }
 
     voteUp() {
-    this.votes ++;
+        this.votes++;
     }
     voteDown() {
-    this.votes --;
+        this.votes--;
+    }
+
+    domain(): string {
+        try {
+            // e.g. http://foo.com/path/to/bar
+            const link: string = this.link.split('//')[1];
+            // e.g. foo.com/path/to/bar
+            return link.split('/')[0];
+        } catch (err) {
+            return null;
+        }
     }
 
 }
