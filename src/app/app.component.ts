bd410d33a087273f692fb07b434e317ffa58f869
grok23 <46896171+grok23@users.noreply.github.com>
2020-02-15 16:00:03 +0000

    now sorts by name when points are equal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Article } from "./article/article.model";
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ export class AppComponent {
   constructor() {
     this.articles = [
       new Article('Angular', 'http://angular.io', 5),
+      new Article('Aaron', 'https://www.aarons.com/', 10),
       new Article('Google', 'http://google.ie', 11),
       new Article('JSON', 'https://www.json.org/json-en.html', 12),  //interestingly, due to the "or 10" votes used in the constructor, setting a base of zero votes here causes a jump to 10 votes
       new Article('DuckDuckGo', 'https://duckduckgo.com/', 16),
@@ -27,7 +29,12 @@ export class AppComponent {
     return false;
   }
   sortedArticles(): Article[] {
-    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
-  }
+    //return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);  //sorts by points value
 
+    //return this.articles.sort((a: Article, b: Article) => a.title.localeCompare(b.title)); //will sort by article name in alphabetical order
+
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes || a.title.localeCompare(b.title));//sorts by points order and alphabetical order when points are equal
+
+  }
 }
+
