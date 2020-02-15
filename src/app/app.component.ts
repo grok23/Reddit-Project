4373afcb974bcd24044fc494d7c601b307dc17f8
grok23 <46896171+grok23@users.noreply.github.com>
2020-02-15 13:42:00 +0000

    finished part 5, added more sites

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,16 @@ export class AppComponent {
     this.articles=[
       new Article('Angular','http://angular.io',5),
       new Article('Google','http://google.ie',11),
-      new Article('TikTok','http://tiktok.com',-1)  //interestingly, due to the "or 10" votes used in the constructor, setting a base of zero votes here causes a jump to 10 votes
+      new Article('TikTok','http://tiktok.com',-1),  //interestingly, due to the "or 10" votes used in the constructor, setting a base of zero votes here causes a jump to 10 votes
+      new Article('DuckDuckGo','https://duckduckgo.com/',16),
+      new Article('Pi-Hole','https://pi-hole.net/',25)
     ];
   }
 
   addArticle(title:HTMLInputElement, link:HTMLInputElement):boolean {
+  this.articles.push(new Article(title.value, link.value,0));
+  title.value=' ';
+  link.value=' '; 
   console.log(`Adding Article Title${title.value} : and Adding Article Link ${link.value}: `);
   return false;
   }
