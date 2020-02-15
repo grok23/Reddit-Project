49d4c84d78442bce5317bee05f74bbdcf87a106b
grok23 <46896171+grok23@users.noreply.github.com>
2020-02-15 12:49:37 +0000

    article component added

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+
+  votes:number;
+  title:string;
+  link:string;
+
+  constructor() {
+    this.title="Angular";
+    this.link="http://angular.io";
+    this.votes=10;
+   }
+
+  ngOnInit(): void {
+  }
+
+  voteUp() {
+    this.votes ++;
+  }
+  voteDown() {
+    this.votes --;
+  }
+
+}
